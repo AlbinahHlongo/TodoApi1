b0221695b7ae3ee2d2d1d2d7e76d9eb104bb7497
Albinah <albinankateko@gmail.com>
2022-02-15 16:38:24 +0200

    Scaffold a controller

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore;
+//using System.Data;
+//using System.Data.SqlTypes;
+//using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
